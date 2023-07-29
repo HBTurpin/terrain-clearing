98868503aa55f4af2f67ae7bae426e796343a984
johnb432 <58661205+johnb432@users.noreply.github.com>
2023-07-29 08:53:15 +0200

    Update CfgVehicles.hpp

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -3,15 +3,15 @@ class CfgVehicles {
     class CAManBase: Man {
         class ACE_SelfActions {
             class ACE_Equipment {
-				class ClearBrush {
-					displayName = "Clear Brush";
-					condition = QUOTE(_player call FUNC(canClearBrush));
-					// Wait a frame to handle "Do When releasing action menu key" option
-					statement = QUOTE([ARR_2({_this call FUNC(clearBrush)},_player)] call CBA_fnc_execNextFrame);
-					exceptions[] = {};
-					showDisabled = 0;
-					icon = "\a3\modules_f\data\hideterrainobjects\icon32_ca.paa";
-				};
+                class GVAR(clearBrush) {
+                    displayName = "Clear Brush";
+                    condition = QUOTE(_player call FUNC(canClearBrush));
+                    // Wait a frame to handle "Do When releasing action menu key" option
+                    statement = QUOTE([ARR_2({_this call FUNC(clearBrush)},_player)] call CBA_fnc_execNextFrame);
+                    exceptions[] = {};
+                    showDisabled = 0;
+                    icon = "\a3\modules_f\data\hideterrainobjects\icon32_ca.paa";
+                };
             };
         };
     };
